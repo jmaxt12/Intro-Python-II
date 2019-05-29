fc1de0b1283db7e363aedb9c3a05d1a857ab244f
Jake Thomas <jmaxt12@gmail.com>
2019-05-29 15:45:28 -0600

    crated a player

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, current_room, item):
+        self.name = name
+        self.current_room = current_room
+        self.item = item
+    def __str__(self):
+        return str(self.__dict__)
+
+print(Player("Phil", "bedroom", "knife"))
